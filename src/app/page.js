4cdaae61542155586cac13845d0182e51c18da50
akshita141105 <akshitapandey1411@gmail.com>
2026-08-01 20:42:56 +0530

    Fix caching issue causing stale login redirect

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
 import { redirect } from "next/navigation";
 import { cookies } from "next/headers";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const cookieStore = await cookies();
   const accessToken = cookieStore.get("accessToken")?.value;
